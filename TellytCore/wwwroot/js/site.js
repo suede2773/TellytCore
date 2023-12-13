943aff5f81c2e27a3f7958d48d7c86afb402dd4d
Craig Green <craig.mobsters@gmail.com>
2023-12-12 20:20:39 -0700

    Login updates

--- TellytCore/wwwroot/js/site.js
+++ TellytCore/wwwroot/js/site.js
@@ -2,19 +2,59 @@
 var userEmail = "";
 var displayName = "";
 
+$("#btnLogin").click(function () {
+  $("#loadingLogin").show();
+  if (ValidateLoginFields()) {
+    ProcessLogin($("#loginEmail").val(), $("#loginPassword").val());
+  }
+  $("#loadingLogin").hide();
+});
+
+ClearLoginValidation = async () => {
+  $("#loginEmail").removeClass("is-invalid");
+  $("#loginPassword").removeClass("is-invalid");
+  $("#loginErrorRow").hide();
+}
+
+
+
+ValidateLoginFields = () => {
+  ClearLoginValidation();
+
+  if ($("#loginEmail").val().length == 0) {
+    $("#loginEmail").addClass("is-invalid");
+    $("#loginErrorMessage").html("Please provide your email");
+    $("#loginErrorRow").show();
+    return false;
+  }
+
+  if ($("#loginPassword").val().length == 0) {    
+    $("#loginPassword").addClass("is-invalid");
+    $("#loginErrorMessage").html("Please provide a password");
+    $("#loginErrorRow").show();
+    return false;
+  }
+  return true;
+}
+
+ResetLoginModal = async () => {
+  $("#loginEmail").val("");
+  $("#loginPassword").val("");
+  $("#loginErrorRow").hide();
+  $("#loadingLogin").hide();
+}
+
 ProcessLogout = async () => {
   isAuthenticated = false;
   sessionStorage.removeItem("isAuthenticated");
   sessionStorage.removeItem("userEmail");
   sessionStorage.removeItem("displayName");
-  //$("#liGreet").html("");
-  //$("#liGreet").hide();
   CheckAuthentication();
 }
 
 ProcessLogin = async (email, password) => {
-  $("#inputEmail").val("");
-  $("#inputPassword").val("");
+  $("#loginEmail").val("");
+  $("#loginPassword").val("");
 
   let formData = new FormData();
   const authenticateInfo = {
@@ -44,7 +84,7 @@ ProcessLogin = async (email, password) => {
     CheckAuthentication();
   }
   else {
-    $("#checkModalReturnMessage").html(returnAuthData.message);
+    $("#checkLoginReturnMessage").html(returnAuthData.message);
   }
 }
 
@@ -59,32 +99,27 @@ FillAndShowGreeting = async (dislayName) => {
 }
 
 CheckAuthentication = async () => {
-  //$("#divEvents").hide();
+  const loginModal = bootstrap.Modal.getOrCreateInstance($('#loginModal'));
   if (!isAuthenticated) {
     let isAuthenticatedData = sessionStorage.isAuthenticated;
     if (isAuthenticatedData == undefined) {
-      $("#divAuthenticate").show();
+      loginModal.show();
       $("#liLogout").hide();
     }
     else {
       displayName = sessionStorage.displayName;
       userEmail = sessionStorage.userEmail;
       isAuthenitcated = true;
-      $("#divAuthenticate").hide();
+      loginModal.show();
       $("#liLogout").show();
       FillAndShowGreeting(displayName);
-      //$("#divEvents").show();
-      //GetEventData();
     }
   }
   else {
     displayName = sessionStorage.displayName;
     $("#liLogout").show();
     $("#liGreet").show();
-    $("#divAuthenticate").hide();
-
-    //$("#divEvents").show();
-    //GetEventData();
+    loginModal.hide();
   }
 }
 
