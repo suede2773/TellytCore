a605014cebe878422a76731a30571cb8d48d9a3c
nf-tw <63173805+nf-tw@users.noreply.github.com>
2024-02-23 13:09:03 -0800

    Sales

---
+++ TellytCore/Controllers/SalesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TellytCore.Controllers
+{
+    public class SalesController : Controller
+    {
+        // GET: Sales
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
